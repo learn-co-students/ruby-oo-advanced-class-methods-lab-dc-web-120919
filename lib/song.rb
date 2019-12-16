7e3b41b23dbcd23346e88809bd7d6792af36807e
Anna Dybas <a.m.dybas@gmail.com>
2019-12-16 09:52:48 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,62 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find do |song_array|
+      song_array.name == song_name
+    end
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == nil
+      self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song|song.name}
+  end
+
+  def self.new_from_filename(filename)
+    filename_array = filename.split(" - ") #["Thundercat", "For Love I Come.mp3"]
+    parsed_filename_array = filename_array.map {|song_name| song_name.gsub(/.mp3/, '')}
+    song = self.new
+    song.name = parsed_filename_array[1]
+    song.artist_name = parsed_filename_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    filename_array = filename.split(" - ") #["Thundercat", "For Love I Come.mp3"]
+    parsed_filename_array = filename_array.map {|song_name| song_name.gsub(/.mp3/, '')}
+    song = self.new
+    song.name = parsed_filename_array[1]
+    song.artist_name = parsed_filename_array[0]
+    song.save
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 end
